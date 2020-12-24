21ea640fdda11be8998dd9f780e7c50cdb262a40
Edelmir <muratkanov02@gmail.com>
2020-12-24 09:47:45 +0600

    Залил сайт на хаб

---
+++ main.js
@@ -0,0 +1,19 @@
+"use strict";
+let btn = document.querySelector(".btn");
+let ans = document.querySelector(".answer h1");
+
+btn.onclick = function () {
+    let inputs = document.querySelectorAll(".textbox input");
+    let selects = document.querySelectorAll(".textbox select");
+    let top = 0;
+    let bottom = 0;
+    for (let i = 0; i < 10; i++) {
+        let mark = Number(selects[i].value);
+        let cr = Number(inputs[i].value);
+        if (mark !== 99 && cr !== 0) {
+            top += mark * cr;
+            bottom += cr;
+        }
+    }
+    ans.innerHTML = "Ответ: " + String(top / bottom).slice(0, 4);
+}
